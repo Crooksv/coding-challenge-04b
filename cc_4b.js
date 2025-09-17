bc9f17eec05bbb0f36e88b17dc12d62ab08305b1
Vincent Crooks <crooksv@usf.edu>
2025-09-17 19:52:05 -0400

    commit

---
+++ cc_4b.js
@@ -0,0 +1,12 @@
+///inclass coding assignment 04b
+///Vincent Crooks
+
+const products= [
+    {sku: "p1", name: "Rock", category: "Earth", price: "10.99", inventory: "200"},
+    {sku: "p2", name: "Paper", category: "Tree", price: "5.99", inventory: "500"},
+    {sku: "p3", name: "Scissors", category: "Metal", price: "7.99", inventory: "300"},
+    {sku: "p4", name: "Lizard", category: "Jungle", price: "12.99", inventory: "150"},
+    {sku: "p5", name: "Spock", category: "Space", price: "15.99", inventory: "100"},
+];
+
+///commit
