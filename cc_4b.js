3ed4082087b7f584bf4ace5f2e9cc6450a016048
Vincent Crooks <crooksv@usf.edu>
2025-09-17 20:05:10 -0400

    discount prices

--- cc_4b.js
+++ cc_4b.js
@@ -2,11 +2,32 @@
 ///Vincent Crooks
 
 const products= [
-    {sku: "p1", name: "Rock", category: "Earth", price: "10.99", inventory: "200"},
-    {sku: "p2", name: "Paper", category: "Tree", price: "5.99", inventory: "500"},
-    {sku: "p3", name: "Scissors", category: "Metal", price: "7.99", inventory: "300"},
-    {sku: "p4", name: "Lizard", category: "Jungle", price: "12.99", inventory: "150"},
-    {sku: "p5", name: "Spock", category: "Space", price: "15.99", inventory: "100"},
+    {sku: "p1", name: "Rock", category: "electronics", price: "10.99", inventory: "200"},
+    {sku: "p2", name: "Paper", category: "apparel", price: "5.99", inventory: "500"},
+    {sku: "p3", name: "Scissors", category: "groceries", price: "7.99", inventory: "300"},
+    {sku: "p4", name: "Lizard", category: "household", price: "12.99", inventory: "150"},
+    {sku: "p5", name: "Spock", category: "space", price: "15.99", inventory: "100"},
 ];
 
 ///commit
+
+for (let product of products) {
+    let discount = 0;
+    switch(product.category) {
+        case "electronics":
+            discount = 0.2
+            break;
+        case "houesehold":
+        case "groceries":
+            discount = 0.1
+            break;
+        case "apparel":
+            discount = 0.15
+            break
+            default:
+            discount = 0
+            break
+    }
+}
+
+//commit discounts
